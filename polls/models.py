5c864f6495f2dccfee6c1eaf71105266fa7f0d44
Roman Dmytriv <romatill2@gmail.com>
2016-03-06 20:31:27 +0200

    Customized The Administration Portal

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,22 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils import timezone
+import datetime
+
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
 
 	def __str__(self):
 		return self.question_text
+
+	def was_published_in_last_7_days(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
+
+	was_published_in_last_7_days.admin_order_field = 'pub_date'
+	was_published_in_last_7_days.boolean = True
+	was_published_in_last_7_days.short_description = 'Published This Week?'
 
 class Choice(models.Model):
 	choice_text = models.CharField(max_length=200)
